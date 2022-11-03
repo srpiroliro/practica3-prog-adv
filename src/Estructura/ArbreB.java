ff400f1a0d4beaa6f980d7d45e7f25a6bd728355
PolRubio <apocrb23@gmail.com>
2022-11-03 08:51:10 +0100

    first commit

--- src/Estructura/ArbreB.java
+++ src/Estructura/ArbreB.java
@@ -1,5 +1,104 @@
-package src.Estructura;
+package Estructura;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Stack;
+
 
 public class ArbreB {
-    
+	private class NodeA {
+		String contents;
+		ArbreB yes, no;	
+	
+		NodeA(String contents) {
+			//Constructor 1. Inicialitza als atributys yes i no a null
+		}
+		NodeA(String pregunta, ArbreB a1, ArbreB a2) {
+			//Constructor 2. Crea el node i l'inicialitza amb els par�metres
+		}
+	}
+	// Atributs: Taula de 2 posicions
+	private NodeA[] root;
+
+	/* CONSTRUCTORS */
+	public ArbreB(ArbreB a1, ArbreB a2, String pregunta) {
+		//Constructor 1. Crea un arbre amb una pregunta i dos respostes
+	}
+	public ArbreB() {
+		//Constructor 2. Crea un arbre buit
+	}	
+	public ArbreB(String filename) throws Exception{
+		//Constructor 3. Crea l'arbre amb el contingut donat en un fitxer
+		//El par�metre indica el nom del fitxer
+	}
+
+	/* PUBLIC METHODS */
+	public boolean isEmpty() {
+		//COMPLETE
+	}
+	public void rewind() {
+		//COMPLETE
+	}
+	/* True if the current node is an answer (a leaf) */
+	public boolean atAnswer() {
+		//COMPLETE
+	}
+	/* move current to yes-descendant of itself */
+	public void moveToYes() {
+		//COMPLETE
+	}
+	/* move current to yes-descendant of itself */
+	public void moveToNo() {
+		//COMPLETE
+	}
+	/* get the contents of the current node */
+	public String getContents() {
+		//COMPLETE
+	}
+	 /* Substituir la informaci� del node actual
+	 * per la pregunta donada pel jugador. Previament crear el node que ser� el
+	 * seu fill dret, resposta no encertada, amb la informaci� del node actual.
+	 */
+	public void improve(String question, String answer) {
+		//COMPLETE
+	}
+	private void preorderWrite(BufferedWriter buw) throws Exception {
+		//Imprescindible que la implementaci� sigui recursiva
+	}
+	/* Saves contents of tree in a text file */
+	public void save(String filename) throws Exception {
+		BufferedWriter buw = null;
+		try {
+			buw = new BufferedWriter(new FileWriter(filename));
+			this.preorderWrite(buw);
+			buw.close();
+
+		} catch (IOException e) {
+			System.err.println("saveToTextFile failed: " + e);
+			System.exit(0);
+		}
+	}
+	private NodeA loadFromFile(String filename){
+		//Imprescindible implementaci� recursiva
+	}
+	public void visualitzarAnimals() {
+		/* Following the guidelines indicated in the statement of practice */
+		/* COMPLETE */
+	}
+	public int quantsAnimals() {
+		/* Following the guidelines indicated in the statement of practice */
+		/* COMPLETE */
+	}
+	public int alsada() {
+		/* COMPLETE */
+		// Imprescindible invocar a un m�tode la classe NodeA
+	}
+	public void visualitzarPreguntes() {
+		/* COMPLETE */
+		// Visualitza a pantalla les preguntes
+		// Imprescindible invocar a un m�tode la classe NodeA
+	}
 }
--- src/Joc/Joc.java
+++ src/Joc/Joc.java
@@ -1,5 +1,10 @@
-package src.Joc;
+package Joc;
 
 public class Joc {
-    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
