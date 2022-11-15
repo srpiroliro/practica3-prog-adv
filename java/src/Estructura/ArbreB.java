3ae38b96d4518f0140f04b93e40ada38be8a5c1a
srpiroliro <srpiroliro21@gmail.com>
2022-11-15 21:48:16 +0100

    improve() fully working

--- java/src/Estructura/ArbreB.java
+++ java/src/Estructura/ArbreB.java
@@ -74,9 +74,9 @@ public class ArbreB {
 	}
 	public String getContents(){return (isEmpty())?null:root[1].contents;}
 	public void improve(String question,String answer){
-		root[1].yes=new ArbreB(null,null,answer);
-		root[1].contents=question;
 		root[1].no=new ArbreB(null,null,root[1].contents);
+		root[1].contents=question;
+		root[1].yes=new ArbreB(null,null,answer);
 	}
 	public void save(String filename) throws Exception{
 		BufferedWriter buw=null;
